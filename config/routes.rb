4773a35ee5c9238bb82317666f67c52c1fab0ffa
nguyennhudat <nguyen.nhu.dat@framgia.com>
2018-04-09 16:54:58 +0700

    Add a full title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	 root 'application#hello'
+  get  "users/new"
+  get  "static_pages/home"
+  get  "/help",    to: "static_pages#help", as: "helf"
+  get  "/about",   to: "static_pages#about"
+  get  "/contactme", to: "static_pages#contact"
+  get  "/signup", to: "users#new"
+  root "static_pages#home"
 end

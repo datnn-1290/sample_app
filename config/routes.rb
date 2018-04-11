c740a144c90033e611e3d71b2d2e81144bea0654
nguyennhudat <nguyen.nhu.dat@framgia.com>
2018-04-11 09:14:27 +0700

    chuong 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contactme', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
